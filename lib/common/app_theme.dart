4c7e128242507b4c23c0a209b775309943586863
ujaann <ujanst@gmail.com>
2024-12-20 21:31:19 +0545

    Added app theme for scaffolds,navbar and card

--- lib/common/app_theme.dart
+++ lib/common/app_theme.dart
@@ -6,13 +6,28 @@ ThemeData getApplicationTheme() {
       scaffoldBackgroundColor: lightBrown,
       fontFamily: "Noto Sans",
       appBarTheme: const AppBarTheme(color: lightBlue),
-      cardTheme: const CardTheme(shadowColor: ceramicGray),
-      navigationBarTheme: const NavigationBarThemeData(
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff9fbfdf),
+        foregroundColor: Colors.white,
+        elevation: 10,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      )),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
           backgroundColor: lightBlue,
-          indicatorColor: lightBrown,
+          splashColor: const Color(0xff9fbfdf),
+          elevation: 2,
+          iconSize: 40,
+          foregroundColor: Colors.blueGrey[700]),
+      //notworking
+      cardColor: lightBlue,
+      cardTheme: const CardTheme(shadowColor: ceramicGray, color: ceramicWhite),
+      navigationBarTheme: NavigationBarThemeData(
+          backgroundColor: lightBlue,
+          indicatorColor: const Color(0xff9fbfdf),
           labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
           iconTheme: WidgetStatePropertyAll(
-              IconThemeData(size: 36, color: Colors.blueGrey))),
+              IconThemeData(size: 36, color: Colors.blueGrey[700]))),
       inputDecorationTheme: const InputDecorationTheme(
           filled: true, fillColor: Color(0x99fefefe)));
 }
--- lib/common/gap.dart
+++ lib/common/gap.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-const gap8Y=SizedBox(height: 8,);
-const gap16Y=SizedBox(height: 16,);
-const gap24Y=SizedBox(height: 24,);
+const gap8Y = SizedBox(
+  height: 8,
+);
+const gap16Y = SizedBox(
+  height: 16,
+);
+const gap24Y = SizedBox(
+  height: 24,
+);
+const gap8X = SizedBox(
+  height: 8,
+);
