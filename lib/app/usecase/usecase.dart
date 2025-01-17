2e98393bca442199c36bfe7398cc00af610cba4f
ujaann <ujanst@gmail.com>
2025-01-17 18:47:09 +0545

    create classes for database failures and usecase

---
+++ lib/app/usecase/usecase.dart
@@ -0,0 +1,10 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:kitchening/core/error/failure.dart';
+
+abstract interface class UsecaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
